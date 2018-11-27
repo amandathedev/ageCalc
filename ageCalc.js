7692edf78b47033b15e9cebbbfdb1cc410ed2725
Amanda Treutler <amandathedev@gmail.com>
2018-11-26 20:17:36 -0600

    First commit

---
+++ ageCalc.js
@@ -0,0 +1,4 @@
+var userAge = prompt("How old are you?");
+var numDays = userAge * 365.25;
+var numMins = numDays * 1440;
+alert("You have been alive for roughly " + numDays + " days and " + numMins + " minutes.");
